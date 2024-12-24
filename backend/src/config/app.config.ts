94982eea3fe5226d55bf00212dc7219725bc1cb1
Vlad Epov <vladep622@gmail.com>
2024-12-24 13:05:18 +0200

    improved env handling

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -1,24 +1,9 @@
-function configDev() {
-  return {
-    DB_URL: process.env.DEV_DB_URL,
-    DB_NAME: process.env.DEV_DB_NAME,
-  };
-}
-
-function configProd() {
-  return {
-    DB_URL: process.env.PROD_DB_URL,
-    DB_NAME: process.env.PROD_DB_NAME,
-  };
-}
-
 function config() {
-  const configByEnv =
-    process.env.NODE_ENV === "production" ? configProd : configDev;
   return {
     NODE_ENV: process.env.NODE_ENV,
     PORT: process.env.PORT,
-    ...configByEnv(),
+    DB_URL: process.env.DB_URL,
+    DB_NAME: process.env.DB_NAME,
   };
 }
 
